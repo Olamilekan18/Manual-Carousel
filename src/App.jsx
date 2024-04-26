ecf2274dcc2864bbfd1f2b7349d5cb07e88e217e
Kareem Olamilekan <olamilekankareem717@gmail.com>
2024-04-26 09:06:26 +0100

    modified for new project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import data from "./data"
+import {  useState  } from "react";
+
+function App() {
+  
+  
+  return (
+    <>
+      <div className='accordion'>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data =[
+    {
+        key:"1",
+        id: '1',
+        question: "What's your name?",
+        answer: 'Olamilekan'
+    },
+    {
+        key:"2",
+        id: '2',
+        question: 'Nationality',
+        answer: 'Nigeria'
+    },
+    {
+        key:"3",
+        id: '3',
+        question: 'Date of birth',
+        answer: 'December 18'
+    },
+    {
+        key:"4",
+        id:'4',
+        question: 'Favorite food',
+        answer: 'Beans'
+    }
+]
+
+export default data
